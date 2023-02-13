1e902541604cd8e30a26e155e7269ad43056c53a
@otni.rodrigues <ors@cin.ufpe.br>
2023-02-13 15:53:19 -0300

    Convertendo a data em minutos

--- app/src/main/java/br/ufpe/cin/residencia/calculadordeidade/MainActivity.java
+++ app/src/main/java/br/ufpe/cin/residencia/calculadordeidade/MainActivity.java
@@ -3,9 +3,15 @@ package br.ufpe.cin.residencia.calculadordeidade;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,10 +27,30 @@ public class MainActivity extends AppCompatActivity {
 
         botao.setOnClickListener(
                 v -> {
-                    String dataDigitada = textoEntrada.getText().toString();
-                    textoSaida.setText(dataDigitada);
+                    Editable textDigitado = textoEntrada.getText();
+                    String dataDigitada = textDigitado.toString();
+                    String saida = idadeEmMinutos(dataDigitada);
+                    textoSaida.setText(saida);
 
                 }
         );
+    }
+
+    private String idadeEmMinutos(String data){
+        SimpleDateFormat simple = new SimpleDateFormat("dd/mm/yyyy");
+        String resultado = "";
+        try {
+            Date d = simple.parse(data);
+            long dataEmMinutos = d.getTime()/60000;
+            Date agora = simple.parse(simple.format(System.currentTimeMillis()));
+            long agoraEmMinutos = agora.getTime()/60000;
+            long diferenca = agoraEmMinutos-dataEmMinutos;
+            resultado = String.valueOf(diferenca);
+        }catch (ParseException e){
+            //throw new RuntimeException(e);
+            Toast.makeText(this, "A data fornecida não está no formato correto (DD/MM/YYYY).",
+                    Toast.LENGTH_LONG).show();
+        }
+        return resultado;
     }
 }
